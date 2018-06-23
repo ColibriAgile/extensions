044e03faa4d9f80f28b31c2a650c1010e88f49f4
Pitty <pitty.d@ncr.com>
2018-06-22 21:04:48 -0300

    Adicionada soluçao C#

---
+++ night_club/_build/extensao_client.iss
@@ -0,0 +1,28 @@
+#ifndef AppName
+  #define AppName "Nome"
+  #define AppVersion "1.0.0.0"
+  #define ExtensionName "Nome"
+#endif
+
+#define PastaIncludes ".\"
+#include PastaIncludes + "colibri_includes.iss"
+
+
+[Setup]
+AppCopyright=Copyright 2017
+AppName={#AppName}
+AppVersion={#AppVersion} 
+AppPublisher=NCR Corporation
+DefaultDirName={code:PastaNCRColibri|c:\NCR Colibri}
+OutputDir=_build\pacote
+OutputBaseFilename={#ExtensionName}_{#AppVersion}_client
+Uninstallable=no
+UsePreviousAppDir=no
+SourceDir=..
+
+[InstallDelete]
+Type: filesandordirs; Name: "{app}\plugins\{#ExtensionName}\templates"
+
+
+[Files]
+Source: "client\templates\*"; DestDir: "{app}\plugins\{#ExtensionName}\templates"; Flags: skipifsourcedoesntexist ignoreversion recursesubdirs restartreplace overwritereadonly; Excludes: "leia-me.txt"
---
+++ night_club/source/Plugin.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+// O assembly do plugin deve ser Plugin.[NomeDoPlugin]
+// O namespace aqui deve ser Plugin[NomeDoPlugin]
+namespace PluginNightClub
+{
+  class DadosDoFabricante
+  {
+    public class Fabricante
+    {
+      public string empresa = "NCR";
+      public string desenvolvedor = "NCR Colibri";
+      public string termos_da_licenca = "";
+      public string direitos_de_copia = "";
+      public string marcas_registradas = "";
+    }
+    public class Suporte
+    {
+      public string email = "";
+      public string url = "";
+      public string telefone = "";
+    }
+    public Fabricante fabricante;
+    public Suporte suporte;
+    public DadosDoFabricante()
+    {
+      fabricante = new Fabricante();
+      suporte = new Suporte();
+    }
+    public string ToJson()
+    {
+      JavaScriptSerializer serializer = new JavaScriptSerializer();
+      return serializer.Serialize(this);
+    }
+  }
+
+  public class Plugin
+  {
+    /******************************************
+     * 
+     * Funções obrigatórias
+     * 
+     ******************************************/
+    public static string ObterNome()
+    {
+      return "Night Club";
+    }
+
+    public static string ObterVersao()
+    {
+      return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+
+    public static string ObterDadosFabricante()
+    {
+      DadosDoFabricante dados = new DadosDoFabricante();
+      dados.fabricante.empresa = "NCR";
+      dados.fabricante.desenvolvedor = "NCR Labs";
+      dados.suporte.email = "";
+      return dados.ToJson();
+    }
+
+    /******************************************
+     * 
+     * Funções opcionais
+     * 
+     ******************************************/
+    public static void Configurar(string maquinas)
+    {
+      Colibri.MostrarMensagem("teste", Colibri.TipoMensagem.aviso, "Titulo", "sim", "direita");
+      FormConfig testDialog = new FormConfig();
+      testDialog.ShowDialog();
+      testDialog.Dispose();
+    }
+
+    public static void ConfigurarDB(string servidor, string banco, string usuario, string senha, string provedor)
+    {
+
+    }
+    public static void Ativar(int umaMaquina)
+    {
+    }
+    public static void Desativar(int umaMaquina)
+    {
+    }
+    public static void ObterMacro(string umaMacro)
+    {
+    }
+    public static string Notificar(string sEvento, string sContexto)
+    {
+      // Aqui você é notificado dos eventos
+      dynamic contexto = JObject.Parse(sContexto);
+
+      return "";
+    }
+    public static void RegistrarAssinaturas()
+    {
+      // Aqui você assina os eventos
+      Colibri.AssinarEvento("EventoDeSistema.PluginsCarregados");
+    }
+
+    public static string RegistrarPermissoes()
+    {
+      return "";
+    }
+  }
+}
