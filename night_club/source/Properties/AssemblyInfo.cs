3c6bbeafcc2681722261b30f73bd2f6359669930
Pitty <pitty.d@ncr.com>
2018-07-19 10:49:40 -0300

    Auto stash before merge of "develop" and "origin/develop"

--- night_club/source/Properties/AssemblyInfo.cs
+++ night_club/source/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CheckinExtended")]
+[assembly: AssemblyTitle("NightClubPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CheckinExtended")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("NightClubPlugin")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
