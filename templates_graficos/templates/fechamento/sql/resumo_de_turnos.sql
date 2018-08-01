1be24c43e48e572bb05be13708909b60e2797ac8
Andre Foresti <andre.foresti@ncr.com>
2018-08-01 16:21:20 -0300

    DSK-5047 ajuste no sql da sessão resumo de turnos

--- templates_graficos/templates/fechamento/sql/resumo_de_turnos.sql
+++ templates_graficos/templates/fechamento/sql/resumo_de_turnos.sql
@@ -1,7 +1,6 @@
 
 
 declare 
-  @dt_contabil datetime = (select valor from cache.data_contabil),
   @dt datetime = /*macro:dataini*/+''
 
 
@@ -24,31 +23,16 @@ left join dbo.funcionario f on f.id = t.func_id
 --where t.dt_contabil = '2018-06-16'
 /*macro:filtro*/
 
-/* do dia */
-if @dt = @dt_contabil 
   select 
     turno = t.turno_id,
     oper = min(t.oper),
     dt_hr_ini = min(t.dt_hr_abertura),
     dt_hr_fim = min(t.dt_hr_fechamento),
-    total= sum(isnull(mc.vl_recebido,0))
+    total= sum(isnull(f.valor_venda,0))
   from @turnos t
-  left join dbo.movimento_caixa mc on mc.turno_id = t.turno_id
-  where mc.cancelado = 0
-  group by t.turno_id
+  join dbo.fn_fechamento_caixa(@dt, @dt, -1, 1, -1, '') f on t.turno_id = f.turno
+  group by turno_id
 
-/* geral*/
-else 
-  select 
-    turno = t.turno_id,
-    oper = min(t.oper),
-    dt_hr_ini = min(t.dt_hr_abertura),
-    dt_hr_fim = min(t.dt_hr_fechamento),
-    total= sum(isnull(mc.vl_recebido,0))
-  from @turnos t  
-  left join dbo.movimento_caixa_geral mc on mc.turno_id = t.turno_id
-  where mc.cancelado = 0
-  group by t.turno_id
 
 /*mapeamentos
   periodo=t.dt_contabil
