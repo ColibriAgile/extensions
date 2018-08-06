9624b0aeaf4a537d01e0fc8f1e6238c293ce6f1b
Douglas Cunha <douglas.cunha@ncr.com>
2018-08-06 17:00:36 -0300

    LAB-10: Nova seção de atendimento de mesas por atendentes.

---
+++ templates_graficos/templates/fechamento/sql/atendimento_mesas_por_atendente.sql
@@ -0,0 +1,14 @@
+﻿declare @dt datetime = /*macro:dataini*/;
+
+select
+  atendente = f.nome,
+  qtd_mesas = count(v.venda_id),
+  qtd_pessoas = sum(o.qtd_pessoas),
+  total = sum(o.vl_total),
+  media_mesa = avg(o.vl_total),
+  media_cliente = sum(o.vl_total) / sum(o.qtd_pessoas)
+from dbo.fn_operacoes_venda(@dt, @dt, 0) o
+cross apply dbo.fn_venda(o.operacao_id, default, 1) v
+join dbo.funcionario f with (nolock) on f.id = v.func_atendeu_id
+where o.modo_venda_id = 3
+group by f.nome
