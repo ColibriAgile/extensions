8730dc04ff9e79f732cdf1c527cc88f3c181289d
Douglas Cunha <douglas.cunha@ncr.com>
2018-10-03 17:50:35 -0300

    Ajustes de formatação.

--- eventos/src/Colibri.cs
+++ eventos/src/Colibri.cs
@@ -13,10 +13,13 @@ namespace PluginEventos
         #region Metodos
         public static void AssinarEvento(string evento) 
             => ((Action<string>) dictFuncoes["AssinarEvento"])(evento);
+
         public static void Callback(string evento, string contexto) 
             => ((Action<string, string>) dictFuncoes["Callback"])(evento, contexto);
+
         public static void GravarConfig(string config, int maquinaId, string valor) 
             => ((Action<string, int, string>) dictFuncoes["GravarConfig"])(config, maquinaId, valor);
+
         public static int MostrarMensagem(string mensagem, TipoMensagem tipo, string titulo = "", string botaoPadrao = "nao", string alinhamento = "esquerda", string id = "")
         {
             string dados = $"{{\"mensagem\":\"{mensagem}\", \"tipo\":\"{tipo.ToString()}\", \"titulo\":\"{titulo}\", \"alinhamento\": \"{alinhamento}\", \"id\": \"{id}\"}}";
@@ -24,12 +27,16 @@ namespace PluginEventos
         }
         public static int MostrarMensagem(string dados) 
             => ((Func<string, int>) dictFuncoes["MostrarMensagem"])(dados);
+
         public static string MostrarTeclado(string dados) 
             => ((Func<string, string>) dictFuncoes["MostrarTeclado"])(dados);
+
         public static string ObterConfigs(int maquina) 
             => ((Func<int, string>) dictFuncoes["ObterConfigs"])(maquina);
+
         public static int VerificarPermissao(string GUID, int elevar) 
             => ((Func<string, int, int>) dictFuncoes["VerificarPermissao"])(GUID, elevar);
+
         public static void AtribuirFuncoes(Dictionary<string, object> dictFuncoes)
             => Colibri.dictFuncoes = dictFuncoes;
         #endregion
