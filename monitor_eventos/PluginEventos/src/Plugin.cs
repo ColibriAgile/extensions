e4ae8009c41d06962fa15adc8f647856d2f515d5
Andrey Santanna <andrey.santanna@ncr.com>
2019-05-07 16:25:56 -0300

    troca para chave_extensao

--- monitor_eventos/PluginEventos/src/Plugin.cs
+++ monitor_eventos/PluginEventos/src/Plugin.cs
@@ -64,7 +64,7 @@ namespace PluginEventos
             => new DadosDoFabricante().ToJson();
         
         public static string ObterDadosLicenca(string info)
-            => "{\"chave_assinatura\": \"79701D1D-FA1C-44CD-A789-6E867D8FBC23\"}";
+            => "{\"chave_extensao\": \"79701D1D-FA1C-44CD-A789-6E867D8FBC23\"}";
 
         public static void Configurar(string maquinas)
         {
